11d2a57c0845ccace41462ce74387bf83dd3e27d
Joseph <40335314+JRedOW@users.noreply.github.com>
2024-10-15 19:28:35 +0000

    API Session Destruction and Enviromental Variables

--- src/Promul.Server~/Controllers/SessionController.cs
+++ src/Promul.Server~/Controllers/SessionController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Promul.Relay.Server.Models.Sessions;
 using Promul.Relay.Server.Relay;
 
+using System.Security.Cryptography;
+
 namespace Promul.Relay.Server.Controllers;
 
 [ApiController]
@@ -21,15 +23,52 @@ public class SessionController : ControllerBase
     public SessionInfo CreateSession()
     {
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int joinCodeLength = 6;
 
-        //var joinCode = new string(Enumerable.Repeat(chars, 6).Select(s => s[RandomNumberGenerator.GetInt32(s.Length)]).ToArray());
-        var joinCode = "TEST";
+        if (System.Environment.GetEnvironmentVariable("JOIN_CODE_LENGTH") != null)
+        {
+            if (Int32.TryParse(System.Environment.GetEnvironmentVariable("JOIN_CODE_LENGTH"), out int length))
+            {
+                if (length <= 0) {
+                    _logger.LogInformation("Not using JOIN_CODE_LENGTH enviroment variable value: `{}`, below zero",
+                        length);
+                }
+
+                joinCodeLength = length;
+            } 
+            else
+            {
+                _logger.LogInformation("Not using JOIN_CODE_LENGTH enviroment variable: `{}`, not an integer",
+                    System.Environment.GetEnvironmentVariable("JOIN_CODE_LENGTH"));
+            }
+        }
+
+        var joinCode = new string(Enumerable.Repeat(chars, joinCodeLength).Select(s => s[RandomNumberGenerator.GetInt32(s.Length)]).ToArray());
+        //var joinCode = "TEST";
+
+        // Verify Join Code is unique
+        // Not the best implementation, but would only cause real theoretical problems with around 26^6/6 (51 million) lobbies
+        // If that's somehow an issue, just expand join code length using env var JOIN_CODE_LENGTH
+        while (_relay.GetSession(joinCode) != null) 
+        {
+            joinCode = new string(Enumerable.Repeat(chars, joinCodeLength).Select(s => s[RandomNumberGenerator.GetInt32(s.Length)]).ToArray());
+        }
+
         _relay.CreateSession(joinCode);
+
+        int relayPort = 15593;
+        if (
+            System.Environment.GetEnvironmentVariable("RELAY_PORT") != null && 
+            Int32.TryParse(System.Environment.GetEnvironmentVariable("RELAY_PORT"), out int port)
+        )
+        {
+            relayPort = port;
+        }
         var sci = new SessionInfo
         {
             JoinCode = joinCode,
-            RelayAddress = "aus628.relays.net.fireworkeyes.com",
-            RelayPort = 15593
+            RelayAddress = System.Environment.GetEnvironmentVariable("RELAY_ADDRESS") ?? "aus628.relays.net.fireworkeyes.com",
+            RelayPort = relayPort
         };
 
         _logger.LogInformation("User {}:{} created session with join code {}",
@@ -45,12 +84,47 @@ public class SessionController : ControllerBase
     {
         var session = _relay.GetSession(joinCode.JoinCode);
         if (session == null) return NotFound();
+
+        int relayPort = 15593;
+        if (
+            System.Environment.GetEnvironmentVariable("RELAY_PORT") != null && 
+            Int32.TryParse(System.Environment.GetEnvironmentVariable("RELAY_PORT"), out int port)
+        )
+        {
+            relayPort = port;
+        }
         return new SessionInfo
         {
             JoinCode = session.JoinCode,
-            RelayAddress = "aus628.relays.net.fireworkeyes.com",
-            RelayPort = 15593
+            RelayAddress = System.Environment.GetEnvironmentVariable("RELAY_ADDRESS") ?? "aus628.relays.net.fireworkeyes.com",
+            RelayPort = relayPort
         };
+    }
+
+    [HttpDelete("Destroy")]
+    public ActionResult<SessionInfo> DestroySession([FromBody] SessionRequestJoinInfo joinCode)
+    {
+        // For backwards compatability and safety if API public
+        if (System.Environment.GetEnvironmentVariable("ENABLE_DESTROY_API") != null && !(
+            System.Environment.GetEnvironmentVariable("ENABLE_DESTROY_API") == "true" || 
+            System.Environment.GetEnvironmentVariable("ENABLE_DESTROY_API") == "t" ||
+            System.Environment.GetEnvironmentVariable("ENABLE_DESTROY_API") == "1" )
+        )
+        {
+            return NotFound();
+        }
+
+        var session = _relay.GetSession(joinCode.JoinCode);
+        if (session == null) return NotFound();
+
+        _logger.LogInformation("User {}:{} destroyed session with join code {}",
+            HttpContext.Connection.RemoteIpAddress,
+            HttpContext.Connection.RemotePort,
+            joinCode.JoinCode);
+
+        _relay.DestroySession(session);
+        
+        return Ok();
     }
 
     public struct SessionRequestJoinInfo
